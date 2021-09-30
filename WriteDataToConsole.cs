a3e33297f87bd97e7e723a81d22cc084aa9498d8
jsipola <juuso_sipola@hotmail.com>
2021-09-30 23:14:28 +0300

    Fixed comment

--- WriteDataToConsole.cs
+++ WriteDataToConsole.cs
@@ -30,7 +30,7 @@ namespace ExcelToJsonExtractor
             Console.WriteLine(headerLine);
             Console.WriteLine(separatorLine);
 
-            /* TODO: improve this to order the trades alphabetically */
+            /* TODO: improve this for better performance */
             var groupedList = trades.OrderBy(info => info.Name).GroupBy(trade => trade.Name, item => item);
             foreach (var item in groupedList)
             {
